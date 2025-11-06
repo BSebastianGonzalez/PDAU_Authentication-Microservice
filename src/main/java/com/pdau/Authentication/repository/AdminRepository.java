d6f055e4d745ff0d9fc8bd03b8239b1129c07cc5
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-11-05 19:12:31 -0500

    feat: added endpoint for special admins email consult

--- src/main/java/com/pdau/Authentication/repository/AdminRepository.java
+++ src/main/java/com/pdau/Authentication/repository/AdminRepository.java
@@ -1,10 +1,13 @@
 package com.pdau.Authentication.repository;
 
 import com.pdau.Authentication.model.Admin;
+import com.pdau.Authentication.model.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByCorreo(String correo);
+    List<Admin> findByRol(Rol rol);
 }
